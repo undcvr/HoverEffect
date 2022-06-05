32ae03354b836a7b2b4d59f3638c3df7fc6e09ef
Diyor <diyor.iknu@gmail.com>
2022-06-05 18:04:54 +0500

    added plume animation

--- script/main.js
+++ script/main.js
@@ -19,7 +19,7 @@ $(document).mousemove(function(e) {
 
 let sy = $('.sy')
 
-let syArray = ' .1uitPGHTC'
+let syArray = ' .1uitPOC'
 
 sy.hover(function() {
     let indx = $(this).index()
@@ -31,8 +31,18 @@ sy.hover(function() {
             $(sy[indx]).text(syArray[i])
             $(sy[indx - 1]).text(syArray[i])
             $(sy[indx + 1]).text(syArray[i])
+
+            $(sy[indx]).css({'color': 'red'})
+            $(sy[indx - 1]).css({'color': 'red'})
+            $(sy[indx + 1]).css({'color': 'red'})
+
+            if (i == syArray.length) {
+                $(sy[indx]).css({'color': 'white'})
+                $(sy[indx - 1]).css({'color': 'white'})
+                $(sy[indx + 1]).css({'color': 'white'})
+            }
         } 
-        setTimeout(changeSys, 200)
+        setTimeout(changeSys, 100)
     }
     changeSys();
 })
